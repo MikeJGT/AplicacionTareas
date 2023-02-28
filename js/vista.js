f6062008016fd879b525ffcfb2f0e3a64f813a48
miky3000 <mikecode808@gmail.com>
2023-02-28 02:57:28 +0100

    Maquetacion banner terminada - falta procesar los datos

--- js/vista.js
+++ js/vista.js
@@ -1,10 +1,14 @@
 const plus = document.querySelectorAll('main section a');
 
-const crear = document.querySelector('crear');
-const wrapper = document.querySelector('.wrapper');
+const crear = document.querySelector('.crearTarea');
+const wrapper = document.querySelector('.wrapperCrear');
+const formCrear = document.querySelector('.wrapperCrear #creaTarea');
 
 function crearElemento(event) {
-    console.log('Evento', event.currentTarget);
+    console.log('Evento', wrapper);
+
+    // wrapper.style.display = 'block';
+
 }
 
 plus.forEach(item => item.addEventListener('click', crearElemento))
